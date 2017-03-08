2b65524d0510622adf1171fe1cc5f11c5fe9eb95
Mouli <crmouli@github.com>
2017-03-08 06:46:44 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# javaSamples
+# Java Sample programs
+
---
+++ src/main/java/org/samples/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.samples;
+
+public class HelloWorld {
+    public static void main(String... argvs) {
+        System.out.println("Hello World!");
+    }
+}
